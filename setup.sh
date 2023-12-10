74d227f83cebf566cc619aa3be132901e1f37fd8
Orazio Spina <80155267+Oradzie@users.noreply.github.com>
2023-12-10 17:23:08 +0100

    Fixed error in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 [[ -e ~/.bashrc ]] && echo '[ -s "${PWD}/project-setup.sh" ] && source "${PWD}/project-setup.sh"' >> ~/.bashrc
-[[ -e ~/.zshrc ]] && echo "[ -s "${PWD}/project-setup.sh" ] && source "${PWD}/project-setup.sh"' >> ~/.zshrc
+[[ -e ~/.zshrc ]] && echo '[ -s "${PWD}/project-setup.sh" ] && source "${PWD}/project-setup.sh"' >> ~/.zshrc
