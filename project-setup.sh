99c9c30d3fe3500e2d4e9ef99879c5fbe60b8d98
Orazio Spina <orazio.spina@studio.unibo.it>
2024-08-12 15:53:24 +0200

    Fixed error in adding method to path

--- project-setup.sh
+++ project-setup.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 function junit_setup() {
-
-	PERCORSO=`find ~/ -name "junit-platform-console-standalone-1.9.1.jar" -print 2>/dev/null`;
-	PERCORSO=${PERCORSO%%.jar*}.jar;
-	
-	[[ ! -d lib ]] && mkdir lib;
-	[[ ! -e ./lib/${PERCORSO##*/} ]] && cp $PERCORSO ./lib
-
-	[[ ! -d src ]] && mkdir src;
-	[[ -d e1 ]] && mv e1 ./src;
-	[[ -d e2 ]] && mv e2 ./src;
-	
-	cd src;
-
-	cd e1;
-	for file in `ls *.java`; do
-		sed -i '' "1s/.*/package e1;/" $file
-	done
-
-	cd ../e2;
-	for file in `ls *.java`; do
-		sed -i '' "1s/.*/package e2;/" $file
-	done
-
-	cd ../..
-	code .
+    
+    PERCORSO=`find ~/ -name "junit-platform-console-standalone-1.9.1.jar" -print 2>/dev/null`;
+    PERCORSO=${PERCORSO%%.jar*}.jar;
+    
+    [[ ! -d lib ]] && mkdir lib;
+    [[ ! -e ./lib/${PERCORSO##*/} ]] && cp $PERCORSO ./lib
+    
+    [[ ! -d src ]] && mkdir src;
+    [[ -d e1 ]] && mv e1 ./src;
+    [[ -d e2 ]] && mv e2 ./src;
+    
+    cd src;
+    
+    cd e1;
+    for file in `ls *.java`; do
+        sed -i '' "1s/.*/package e1;/" $file
+    done
+    
+    cd ../e2;
+    for file in `ls *.java`; do
+        sed -i '' "1s/.*/package e2;/" $file
+    done
+    
+    cd ../..
+    code .
 }
--- setup.sh
+++ setup.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-[[ -e ~/.bashrc ]] && echo '[ -s "${PWD}/project-setup.sh" ] && source "${PWD}/project-setup.sh"' >> ~/.bashrc
-[[ -e ~/.zshrc ]] && echo '[ -s "${PWD}/project-setup.sh" ] && source "${PWD}/project-setup.sh"' >> ~/.zshrc
+# Definisci il percorso dello script di configurazione
+CONFIG_SCRIPT="${PWD}/project-setup.sh"
+
+# Aggiungi la riga ai file di configurazione della shell se non esiste già
+add_source_line() {
+    local rcfile="$1"
+    local line="[ -s \"$CONFIG_SCRIPT\" ] && source \"$CONFIG_SCRIPT\""
+    
+    # Controlla se il file di configurazione esiste
+    if [[ -e "$rcfile" ]]; then
+        # Controlla se la riga è già presente nel file
+        if ! grep -Fxq "$line" "$rcfile"; then
+            echo "$line" >> "$rcfile"
+        fi
+    fi
+}
+
+# Aggiungi la riga a .bashrc e .zshrc
+add_source_line "${HOME}/.bashrc"
+add_source_line "${HOME}/.zshrc"
